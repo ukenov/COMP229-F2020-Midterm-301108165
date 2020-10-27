1068d3566b978f23bda08307ee834736e49ac483
Asylhan Ukenov <ukenov.asylhan@gmail.com>
2020-10-27 18:25:15 -0400

    Change database URI

--- COMP229-F2020-MidTerm-301108165/server/config/db.js
+++ COMP229-F2020-MidTerm-301108165/server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://asylhan:8rX99tFP5zykwFW@mongodbserver.hohag.mongodb.net/midterm?retryWrites=true&w=majority"
 };
