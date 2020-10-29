4f3edbb2e1c9bbdbee96a2b00aea8d24255e105d
Asylhan Ukenov <ukenov.asylhan@gmail.com>
2020-10-28 21:21:00 -0400

    removing comments

--- COMP229-F2020-MidTerm-301108165/server/config/db.js
+++ COMP229-F2020-MidTerm-301108165/server/config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
-  //local MongoDB deployment ->
   "URI": "mongodb+srv://asylhan:8rX99tFP5zykwFW@mongodbserver.hohag.mongodb.net/midterm?retryWrites=true&w=majority"
 };
--- COMP229-F2020-MidTerm-301108165/server/routes/books.js
+++ COMP229-F2020-MidTerm-301108165/server/routes/books.js
@@ -25,10 +25,6 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
     res.render('books/details', {
       title: 'Add Book'
     });
@@ -36,10 +32,6 @@ router.get('/add', (req, res, next) => {
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
     let newBook = book({
       "Title": req.body.title,
       "Price": req.body.price,
@@ -64,10 +56,6 @@ router.post('/add', (req, res, next) => {
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
     let id = req.params.id;
 
     book.findById(id, (err, bookToEdit) => {
@@ -89,10 +77,6 @@ router.get('/:id', (req, res, next) => {
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
     let id = req.params.id
 
     let updatedBook = book({
@@ -120,10 +104,6 @@ router.post('/:id', (req, res, next) => {
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
     let id = req.params.id;
 
     book.remove({_id: id}, (err) => {
